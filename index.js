2fae254600b53d7badb0662c6390e8ae5320b306
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-05-23 17:25:09 -0300

    const clientes

---
+++ index.js
@@ -0,0 +1,9 @@
+const cliente1Nome = "Maria";
+const cliente1CPF = 99988877766;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "João";
+const cliente2CPF = 99988877755;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
