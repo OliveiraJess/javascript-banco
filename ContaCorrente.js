cce14b9d78d9fdd6e0a6cc91f377f9b46ec3d08c
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-05-25 15:47:14 -0300

    class conta, extends e super

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,56 +1,16 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
 // #saldo = 0  -- ainda não esta aprovado esse método para tornar privado, utilizando #
 
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta{
 
-   static numeroDeContas = 0;
-    agencia;
-    _cliente;
-    _saldo = 0
+    static numeroDeContas = 0;
 
-
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
+    constructor(cliente, agencia) {
+        super(0, cliente, agencia)
+        ContaCorrente.numeroDeContas += 1;
     }
 
-    get cliente() {
-        return this._cliente;
-    }
-
-    get saldo() {
-        return this._saldo;
-    }
-
-
-constructor(cliente, agencia) {
-    this.cliente = cliente;
-    this.agencia = agencia;
-    ContaCorrente.numeroDeContas += 1;
-}
-
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if (valor <= 0) {
-            return;
-        }
-        this._saldo += valor;
-    }
-
-    transferir(valor, conta) {
-
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-    }
 };
--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-import{ Cliente }  from "./Cliente.js"
+import { Cliente }  from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 //clientes
 const cliente1 = new Cliente("Maria", 99988877766);
 const cliente2 = new Cliente("João", 99988877755);
 
 
-//contas
-const contaCorrenteCliente1 = new ContaCorrente(cliente1, 1111);
-const contaCorrenteCliente2 = new ContaCorrente(cliente2, 1001);
+//contas correntes
+const contaCorrenteCliente1 = new ContaCorrente(0, cliente1, 1111);
+const contaCorrenteCliente2 = new ContaCorrente(0, cliente2, 1001);
 
 
-//transações
-contaCorrenteCliente1.depositar(500)
-// contaCorrenteCliente1.depositar(100);
-// contaCorrenteCliente1.depositar(150);
-// contaCorrenteCliente1.depositar(100);
-// const valorSacado = contaCorrenteCliente1.sacar(100);
-
-let valor = 300
-contaCorrenteCliente1.transferir(valor, contaCorrenteCliente2)
+//contas poupanças
+const contaPoupancaCliente1 = new ContaPoupanca(50, cliente1, 1111);
+const contaPoupancaCliente2 = new ContaPoupanca(200, cliente2, 1001);
 
 
-//consoles contas
-console.log(contaCorrenteCliente1);
-console.log(contaCorrenteCliente2);
+
+
+//consoles transações
+// contaCorrenteCliente1.depositar(500)
+// contaCorrenteCliente1.sacar(100)
+
+
+
+//consoles contas corrente 
+// console.log(contaCorrenteCliente1);
+// console.log(contaCorrenteCliente2);
 
 //console numero de contas
-console.log(ContaCorrente.numeroDeContas);
+// console.log(ContaCorrente.numeroDeContas);
+
+//consoles contas poupanças
+console.log(contaPoupancaCliente1);
+// console.log(contaPoupancaCliente2);
+
