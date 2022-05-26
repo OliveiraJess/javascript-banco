0e907e470544b52ec827ce47ad4ab397b0a6b175
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-05-26 15:29:50 -0300

    sistema autenticação clientes

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,6 +1,15 @@
 export class SistemaAutenticacao {
     static login(autenticacao, senha) {
-        return autenticacao.autenticar(senha);
+        if (SistemaAutenticacao.autenticavel(autenticacao)) {
 
+            return autenticacao.autenticar(senha);
+        }
+        return false;
+    }
+
+    static autenticavel(autenticacao) {
+        return "autenticar" in autenticacao && autenticacao.autenticar instanceof Function;
     }
 }
+
+
